44b8e3f09b3172e985009bec40823aa2e8f707e4
Jack <vinniemazzetti@gmail.com>
2023-08-29 18:34:11 +0100

    Initial support for macOS

--- generate_project_files.py
+++ generate_project_files.py
@@ -3,3 +3,7 @@ import subprocess
 
 if platform.system() == 'Windows':
   subprocess.run('cmake -B build/Windows')
+if platform.system() == 'Darwin':
+  subprocess.run('cmake -B build/macOS -G Xcode', shell=True)
+else:
+  print("Error: Operating system not supported!")
