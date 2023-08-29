1f08275230c1c1270954983b8fded0b7f6a7d36c
Jack <vinniemazzetti@gmail.com>
2023-08-29 20:14:06 +0100

    Reorder tests

--- src/tests/test_preprocessor.hpp
+++ src/tests/test_preprocessor.hpp
@@ -2,9 +2,14 @@
 
 #include <yak/preprocessor/preprocessor.hpp>
 
-TEST(YakPreprocessor, CanReadSimpleFile) {
-  auto code = yak::Preprocessor::ReadFile("yak/return_zero.yak");
+TEST(YakPreprocessor, CanAvoidCircularInclude) {
+  auto code = yak::Preprocessor::ReadFile("yak/circular_include.yak");
   EXPECT_EQ(code, "func main() { return 0; }");
+}
+
+TEST(YakPreprocessor, CanFormatWhitespace) {
+  auto code = yak::Preprocessor::ReadFile("yak/extra_whitespace.yak");
+  EXPECT_EQ(code, "func main() { int a = 1 + 4; }");
 }
 
 TEST(YakPreprocessor, CanHandleInclude) {
@@ -17,12 +22,7 @@ TEST(YakPreprocessor, CanHandleMultipleIncludes) {
   EXPECT_EQ(code, "func subtract(int a, int b) { return a - b; } func add(int a, int b) { return a + b; } func main() { return subtract(3, add(1, 2)); }");
 }
 
-TEST(YakPreprocessor, CanFormatWhitespace) {
-  auto code = yak::Preprocessor::ReadFile("yak/extra_whitespace.yak");
-  EXPECT_EQ(code, "func main() { int a = 1 + 4; }");
-}
-
-TEST(YakPreprocessor, CanAvoidCircularInclude) {
-  auto code = yak::Preprocessor::ReadFile("yak/circular_include.yak");
+TEST(YakPreprocessor, CanReadSimpleFile) {
+  auto code = yak::Preprocessor::ReadFile("yak/return_zero.yak");
   EXPECT_EQ(code, "func main() { return 0; }");
 }
