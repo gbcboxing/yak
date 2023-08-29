d3b3bfb0dfd15ada0807d130116c25bcc61614af
Jack <vinniemazzetti@gmail.com>
2023-08-29 05:44:40 +0100

    Add call to preprocessor to main

--- src/yak/main.cpp
+++ src/yak/main.cpp
@@ -2,10 +2,13 @@
 
 #include <args.hxx>
 
+#include "preprocessor/preprocessor.hpp"
+
 int main(int argc, char** argv) {
   args::ArgumentParser parser("yak - a lightweight, embeddable programming language.");
   
   /* List of arguments we accept: */
+  args::Positional<std::string> file(parser, "[file]", "The input file");
   args::Flag help(parser, "help", "Display this help menu and exit", {'h', "help"});
   args::Flag version(parser, "version", "Display program version", {'v', "version"});
 
@@ -29,4 +32,15 @@ int main(int argc, char** argv) {
     std::cout << "yak 0.1\n";
     return 0;
   }
+
+  /* Ensure an input file given */
+  if (!file) {
+    std::cout << "No input file\n";
+    return 1;
+  }
+
+  /* No other relevant flags given - interpret and run the file */
+  /* Since no compile flags etc exist yet, just interpret and run the file regardless */
+  auto code = yak::Preprocessor::ReadFile(file.Get());
+  
 }
